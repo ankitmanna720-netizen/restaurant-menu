722a8f8f3bbeed87c1a40582e1cb08dfe557ffe6
ankitmanna720-netizen <ankitmanna720@gmail.com>
2026-01-19 11:39:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("Restaurant menu website is running");
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  background: #f5f5f5;
+}
+
+header {
+  background: #d32f2f;
+  color: white;
+  text-align: center;
+  padding: 20px;
+}
+
+.menu {
+  padding: 20px;
+}
+
+.item {
+  display: flex;
+  justify-content: space-between;
+  background: white;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+button {
+  background: green;
+  color: white;
+  border: none;
+  padding: 12px;
+  font-size: 16px;
+  border-radius: 5px;
+}
