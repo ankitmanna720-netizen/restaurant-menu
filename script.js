9120cb1f8c4d8b895766d8cb2abd32baffaddce0
ankitmanna720-netizen <ankitmanna720@gmail.com>
2026-01-19 18:29:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ function searchItems() {
     }
   });
 }
+function searchItems() {
+  let input = document.getElementById("searchInput").value.toLowerCase();
+  let cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+    let name = card.getAttribute("data-name");
+    card.style.display = name.includes(input) ? "block" : "none";
+  });
+}
+
+function filterItems(category) {
+  let cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+    let itemCategory = card.getAttribute("data-category");
+    card.style.display =
+      category === "all" || itemCategory === category ? "block" : "none";
+  });
+}
